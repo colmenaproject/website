dd6caee230a60822d4b7a8b202102c9c3c38390a
josi <josi@interfaz.co>
2022-06-30 19:16:57 -0300

    Update next.config.js Locales

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  i18n: {
+    // providing the locales supported by your application
+    locales: ["en", "es", "pt"],
+    
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "es"
+  }
+};
