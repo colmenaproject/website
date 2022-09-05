3b6271081550627590affa961eb1927ef21fd35c
josi <josi@interfaz.co>
2022-09-05 10:05:57 -0300

    Update How.js

--- components/Sections/How.js
+++ components/Sections/How.js
@@ -14,7 +14,8 @@ const How = (props) => {
 			: router.locale === "pt"
 			? "Como funciona?"
 			: "";
-	
+
+
 	let subtitle = 
 		router.locale === "en"
 			? "Recycling in Colmena is very easy. Download the App and start registering your recyclable material. When your container arrives at the collection point it is weighed and your reward is credited to the <em>JellyCoinn</em> cryptocurrency. It benefits waste generators and collectors, while providing good quality material to recycling industries."
@@ -24,26 +25,26 @@ const How = (props) => {
 			? "Reciclar em Colmena é muito fácil. Faça o download do aplicativo e comece a registrar seu material. Quando seu recipiente chega ao ponto de coleta, ele é pesado e sua recompensa é creditada à moeda criptográfica <em>JellyCoin</em>. Ela beneficia os geradores e coletores de resíduos, ao mesmo tempo em que fornece material de boa qualidade às indústrias de reciclagem."
 			: "";
 
-		let imgDesktop = 
-			router.locale === "en"
-				? "/images/colmena/infografia-en.jpg"
-				: router.locale === "es"
-				? "/images/colmena/infografia.jpg"
-				: router.locale === "pt"
-				? "/images/colmena/infografia-pt.jpg"
-				: "";
+	let imgDesktop = 
+		router.locale === "en"
+			? "/images/colmena/infografia-en.jpg"
+			: router.locale === "es"
+			? "/images/colmena/infografia.jpg"
+			: router.locale === "pt"
+			? "/images/colmena/infografia-pt.jpg"
+			: "";
 
-		let imgMobile = 
-			router.locale === "en"
-				? "/images/colmena/infografia-en2x.jpg"
-				: router.locale === "es"
-				? "/images/colmena/infografia2x.jpg"
-				: router.locale === "pt"
-				? "/images/colmena/infografia-pt2x.jpg"
-				: "";
+	let imgMobile = 
+		router.locale === "en"
+			? "/images/colmena/infografia-en2x.jpg"
+			: router.locale === "es"
+			? "/images/colmena/infografia2x.jpg"
+			: router.locale === "pt"
+			? "/images/colmena/infografia-pt2x.jpg"
+			: "";
 
-    return (
-        <section id="services" className={props.className}>
+  return (
+      <section id="services" className={props.className}>
 			
 			{/* <!-- Container --> */}
 			<Container>
